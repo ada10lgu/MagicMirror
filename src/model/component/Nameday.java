4865f2bf125ad361488d61ed19f33478e62efb24
Lars Gustafson <lars.gustafson@gmail.com>
2018-01-09 13:29:53 +0100

    Changed file reading to utf8 forced

--- src/model/component/Nameday.java
+++ src/model/component/Nameday.java
@@ -1,9 +1,12 @@
 package model.component;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
-import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,23 +22,26 @@ public class Nameday extends Text {
 		verify(data, "json");
 
 		StringBuilder sb = new StringBuilder();
-		Scanner s = null;
 		File f = new File(data.getString("json"));
 
 		try {
-			s = new Scanner(f);
-			while (s.hasNext()) {
-				sb.append(s.nextLine()).append("\n");
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF-8"));
+			int c;
+			while ((c = br.read()) != -1) {
+				sb.append((char) c);
 			}
-			s.close();
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException("Could not read nameday.json file");
+			br.close();
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Could not read " + f + " file due to encoding errors\n");
+		} catch (IOException e) {
+			throw new RuntimeException("Could not read " + f + " file\n");
+
 		}
 		try {
 			nameday = new JSONObject(sb.toString());
 		} catch (JSONException e) {
 			nameday = new JSONObject();
-			System.err.printf("Could not parse file, %s\nError: %s\n", f,e.getMessage());
+			System.err.printf("Could not parse file, %s\nError: %s\n", f, e.getMessage());
 		}
 	}
 
